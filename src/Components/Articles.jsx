e17d0518c902d7bd67225a31dddb29fc3e677045
AdelRahuma <adel_bugbug@yahoo.com>
2023-02-13 15:34:42 +0000

    e

--- src/Components/Articles.jsx
+++ src/Components/Articles.jsx
@@ -4,10 +4,13 @@ import { Link } from "react-router-dom";
 import "react-bootstrap";
 import { getArticles } from "../Utilis/FetchData";
 import { v4 as uuidv4 } from "uuid";
+import Form from 'react-bootstrap/Form';
 
 export default function Articles() {
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [key,setKeys]=useState([])
+  let fields='';
 
   useEffect(() => {
     getArticles().then((data) => {
@@ -22,7 +25,36 @@ export default function Articles() {
   return (
     <div className="container">
       <div className="row">
-        <h4>List of Articles</h4>
+        <div className="col-4"> </div>
+        
+        <div className="col-4"><h4>List of Articles</h4></div>
+        <div className="col-4"> 
+        <h5>Sort By </h5>
+        {/* //
+        <select onChange={handleCategoryChange}>
+        <option value="selected"> Select a category </option>
+        {category.map((cat) => (
+          <option key={cat.category_name} value={cat.categories}>
+           
+          </option>
+        ))}
+      </select>
+
+        /// */}
+        {   fields= Object.keys(articles[0])}
+       
+        <Form.Select aria-label="select field to sort By">
+        <option value="selected"> Select a category </option>
+        {fields.map(field=>(
+     
+      <option key={field} value={field}>
+         {field}
+        </option>
+      
+        ))} 
+        </Form.Select>
+        </div>
+       
         <br />
       </div>
 
